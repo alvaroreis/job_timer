728ffbd67ce6d42f8fe4aa0057e0c47c452d52e1
Alvaro Barros <alvaroluiz.rb@gmail.com>
2022-06-07 19:45:15 -0300

    feat: configure Isar database

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,12 +1,19 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/core/database/database.dart';
+import 'package:job_timer/app/core/database/database_impl.dart';
 import 'package:job_timer/app/modules/login/login_module.dart';
+import 'package:job_timer/app/services/auth/auth_service.dart';
+import 'package:job_timer/app/services/auth/auth_service_impl.dart';
 
 import 'modules/home/home_module.dart';
 import 'modules/splash/splash_page.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind.lazySingleton<AuthService>((i) => AuthServiceImpl()),
+        Bind.lazySingleton<Database>((i) => DatabaseImpl()),
+      ];
 
   @override
   List<ModularRoute> get routes => [
@@ -16,5 +23,6 @@ class AppModule extends Module {
         ),
         ModuleRoute(LoginModule.route, module: LoginModule()),
         ModuleRoute(HomeModule.route, module: HomeModule()),
+        // ModuleRoute(ProjectModule.route, module: ProjectModule()),
       ];
 }
