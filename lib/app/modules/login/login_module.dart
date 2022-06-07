b522503f0d2127777604b7f7f3cdacca4b09d810
Alvaro Barros <alvaroluiz.rb@gmail.com>
2022-06-07 19:42:28 -0300

    feat: add login with email

--- lib/app/modules/login/login_module.dart
+++ lib/app/modules/login/login_module.dart
@@ -1,16 +1,22 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/modules/login/controller/login_controller.dart';
 import 'package:job_timer/app/modules/login/login_page.dart';
+import 'package:modular_bloc_bind/modular_bloc_bind.dart';
 
 class LoginModule extends Module {
   static String route = '/login';
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        BlocBind.lazySingleton(
+          (i) => LoginController(authService: i()), // authService > appModule
+        ),
+      ];
 
   @override
   List<ModularRoute> get routes => [
         ChildRoute(
           LoginPage.route,
-          child: (context, args) => const LoginPage(),
+          child: (context, args) => LoginPage(controller: Modular.get()),
         ),
       ];
 }
--- lib/app/modules/login/login_page.dart
+++ lib/app/modules/login/login_page.dart
@@ -1,48 +1,76 @@
+import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:job_timer/app/modules/login/controller/login_controller.dart';
+
+import '../../core/ui/images_ui.dart';
 
 class LoginPage extends StatelessWidget {
-  static String route = '/';
-  const LoginPage({Key? key}) : super(key: key);
+  static const String route = '/';
+  final LoginController controller;
+
+  const LoginPage({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Material(
-      child: Container(
-        padding: const EdgeInsets.all(8.0),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0XFF0092B9),
-              Color(0XFF0167B2),
-            ],
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/logo.png',
-              cacheHeight: 250,
-              cacheWidth: 250,
+    return BlocListener<LoginController, LoginState>(
+      bloc: controller,
+      listenWhen: (previous, current) => previous.status != current.status,
+      listener: (context, state) {
+        if (state.status == LoginStatus.failure) {
+          final message = state.errorMessage ?? 'Erro ao realizar login';
+          AsukaSnackbar.alert(message).show();
+        }
+      },
+      child: Material(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0XFF0092B9),
+                Color(0XFF0167B2),
+              ],
             ),
-            SizedBox(height: size.height * .1),
-            SizedBox(
-              width: size.width * .8,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.grey[200],
-                ),
-                onPressed: () {},
-                child: Text(
-                  'Google',
-                  style: Theme.of(context).textTheme.subtitle1,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(ImagesUI.logo),
+              SizedBox(height: size.height * .1),
+              SizedBox(
+                width: size.width * .8,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.grey[200],
+                  ),
+                  onPressed: controller.signIn,
+                  child: Image.asset(ImagesUI.google),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 15),
+              BlocSelector<LoginController, LoginState, bool>(
+                bloc: controller,
+                selector: (state) => state.status == LoginStatus.loading,
+                builder: (context, bool show) {
+                  return Visibility(
+                    visible: show,
+                    child: const Center(
+                      child: CircularProgressIndicator.adaptive(
+                        backgroundColor: Colors.white,
+                      ),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
