f90578b4cb712adb20338da58b920c5f670817a9
Alvaro Barros <alvaroluiz.rb@gmail.com>
2022-06-07 19:48:59 -0300

    feat: add home page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:job_timer/app/modules/home/widgets/header_project_menu.dart';
 
 class HomePage extends StatelessWidget {
   static String route = '/';
@@ -7,10 +8,63 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
+      drawer: const Drawer(
+        child: SafeArea(
+          child: ListTile(
+            title: Text('Sair'),
+          ),
+        ),
       ),
-      body: Container(),
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            title: Text('Projetos'),
+            expandedHeight: 100,
+            toolbarHeight: 100,
+            centerTitle: true,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(15),
+              ),
+            ),
+          ),
+          SliverPersistentHeader(
+            delegate: HeaderProjectMenu(),
+            pinned: true,
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+            Container(
+              color: Colors.red,
+              height: 300,
+            ),
+          ]))
+        ],
+      ),
     );
   }
 }
---
+++ lib/app/modules/home/widgets/header_project_menu.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:job_timer/app/entities/project_status.dart';
+
+class HeaderProjectMenu extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    var size = MediaQuery.of(context).size;
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Container(
+          height: constraints.maxHeight,
+          padding: const EdgeInsets.all(10),
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: size.width * .45,
+                child: DropdownButtonFormField<ProjectStatus>(
+                  items: ProjectStatus.values
+                      .map((e) =>
+                          DropdownMenuItem(value: e, child: Text(e.label)))
+                      .toList(),
+                  onChanged: (valeu) {},
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    contentPadding: const EdgeInsets.all(10),
+                    isCollapsed: true,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: size.width * .45,
+                child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add),
+                    label: const Text('Novo Projeto')),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  double get maxExtent => 80.0;
+
+  @override
+  double get minExtent => 80.0;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
